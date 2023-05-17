81986994819ee5be2c6def5450e66ab4106d8100
josetenshi <joseangel08rojas2003@gmail.com>
2023-05-16 22:57:44 -0400

    arreglo del javas
    boton de menu

--- javascript/javascrip.js
+++ javascript/javascrip.js
@@ -35,8 +35,8 @@ form.addEventListener('submit', function (event) {
 })
  }
  
-document.querySelector("button.button-menu-toggle");
-    addEventListener("click", function() {
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
            document.querySelector(".nav-links").
                       classList.toggle("nav-links-responsive")
                     }
